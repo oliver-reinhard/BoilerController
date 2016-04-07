5c9dd9813204f279159c7c9615805440085c599f
Oliver Reinhard <oliver.reinhard@gmail.com>
2016-04-07 17:17:02 -0300

    Initial Commit

---
+++ Boiler ControllerTests/Boiler_ControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Boiler_ControllerTests.swift
+//  Boiler ControllerTests
+//
+//  Created by Oliver on 2016-04-07.
+//  Copyright © 2016 Oliver. All rights reserved.
+//
+
+import XCTest
+@testable import Boiler_Controller
+
+class Boiler_ControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
